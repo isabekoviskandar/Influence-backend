ddce7662e3bd1bfac97d078775d768b86f74926d
Iskandar Isabekov <iskandarisabrkov@gmail.com>
2026-04-19 23:25:50 +0500

    fix: pest fixed

--- tests/Feature/ExampleTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-test('the application root redirects to dashboard', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(302);
-    $response->assertRedirect('/dashboard');
-});
