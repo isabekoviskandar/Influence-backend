bf403eb14509573dcf53a7a014bd5ac3e9619dbd
Iskandar Isabekov <iskandarisabrkov@gmail.com>
2026-05-24 18:40:23 +0500

    feat: blog for website

---
+++ app/Http/Resources/BlogResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Blog
+ */
+class BlogResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'content' => $this->content,
+            'image' => $this->image,
+            'status' => $this->status,
+            'author' => $this->author,
+        ];
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -75,7 +75,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->email === 'iskandarisabrkov@gmail.com';
+        return true;
     }
 
     public function getMaxChannelsAttribute(): int
