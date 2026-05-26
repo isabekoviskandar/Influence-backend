9340468739ccac7df38c1032d7a41e1f3f024343
Iskandar Isabekov <iskandarisabrkov@gmail.com>
2026-05-26 14:59:14 +0500

    fix: only admin can access the filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -75,7 +75,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return true;
+        return $this->email === 'iskandarisabrkov@gmail.com';
     }
 
     public function getMaxChannelsAttribute(): int
