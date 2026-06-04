1645278ec925d8027a10462e2e2b43d046bfcc10
Iskandar Isabekov <iskandarisabrkov@gmail.com>
2026-06-04 15:24:27 +0500

    chore: api's

--- app/Http/Controllers/Api/ChannelController.php
+++ app/Http/Controllers/Api/ChannelController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\ChannelService;
+use Illuminate\Http\Request;
 
 class ChannelController extends Controller
 {
-    //
+    protected $service;
+
+    public function __construct(ChannelService $channelService)
+    {
+        $this->service = $channelService;
+    }
+
+    public function getUserChannels(Request $request)
+    {
+        return $this->service->getUserChannels($request);
+    }
 }
